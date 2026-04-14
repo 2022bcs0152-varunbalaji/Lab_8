122fde15dfaf6f9e85edfba1d58256f664f073b0
varunbalaji <polyglot1432@gmail.com>
2026-04-14 09:11:06 +0000

    Fixed train.py

--- app/train.py
+++ app/train.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import root_mean_squared_error, r2_score
 
 # Load dataset
-df = pd.read_csv("data/housing.csv")
+df = pd.read_csv("data/housing_5k.csv")
 
 # Handle missing values
 df = df.dropna()
